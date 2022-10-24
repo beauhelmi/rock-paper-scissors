8ed6ddc69eb85b7041141808387d1649c80af9b2
beauhelmi <beauhelmi@gmail.com>
2022-10-24 22:31:47 +0800

    first function. displaying random display

---
+++ index.jsx
@@ -0,0 +1,8 @@
+function getComputerChoice () {
+    let options = ["rock", "paper", "scissors"];
+    let choice = options[Math.floor(Math.random() * options.length)];
+}
+
+// function getSelection(playerSelection, computerSelection) {
+//     return("You Lose! Paper beats Rock");
+// }
