8765b589d1d69170f0c64d72d1f940a87cb9bbfa
beauhelmi <beauhelmi@gmail.com>
2022-10-25 09:13:25 +0800

    finish index.jsx command

--- index.jsx
+++ index.jsx
@@ -77,3 +77,201 @@ function beginningAnimation() {
     };
 }
 
+function fadeIn() {
+    let text = document.querySelector("animate");
+
+    let strText = text.textContent;
+    let splitText = strText.split("");
+    text.textContent = "";
+    
+    for (i = 0; i < splitText.length; i++) {
+        text.innerHTML += `<span>${splitText[i]}</span>`;
+    }
+
+    let char = 0;
+    let timer = setInterval(onTick, 50);
+
+    function onTick() {
+        const span = text.querySelectorAll("span)[char];
+        span.classList.add("fade");
+        char++;
+
+        if(char === splitText.length) {
+            complete();
+        return;        }
+    }
+
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const img = button.querySelector("img");
+        playerSelection = img.alt.toLowerCase();
+
+        playRound(playerSelection, computerSelection);
+
+        if (playerScore === 5 || computerScore === 5) {
+            declareWinner();
+        }
+    });
+});
+
+const myArray = ["Rock","Paper","Scissors"];
+
+function computerPlay() {
+    return myArray[~~(Math.random() * myArray.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection === playerSelection) {
+        displayResults("Tie game!");
+    } else if (
+        (computerSelection === "rock" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "scissors") ||
+        (computerSelection === "paper" && playerSelection === "paper")
+    )
+    {
+        computerScore = ++computerScore;
+        keepCpuScore();
+        if (computerScore === 1) {
+            displayResults(
+                `Oh no! You lost.
+                ${capitalize(computerSelection)} beats ${playerSelection}.`
+            );
+        } else if (computerScore === 2) {
+            displayResults(
+                `Arghh. ${capitalize(computerSelection)} beats ${playerSelection}. Give it another 
+                shot!`
+            );
+        } else if (computerScore === 3) {
+            displayResults(
+                `${capitalize(computerSelection)} beats ${playerSelection}. It's okay. You got this!`
+            );
+        } else if (computerScore === 4) {
+            displayResults(
+                `Oh no! It's match point!! ${capitalize(computerSelection)} beats ${playerSelection}. Don't let us down!!`
+            );
+        } else {
+            displayResults(`${computerSelection} beats ${playerSelection}`);
+        }
+    } else {
+        playerScore = ++playerScore;
+        keepPlayerScore();
+        if (playerScore === 1) {
+            displayResults(
+                `Let's go!!! You won.
+                ${capitalize(playerSelection)} beats ${computerSelection}`
+            );
+        } else if (playerScore === 2) {
+            displayResults(
+                `You're pretty good at this. ${capitalize(playerSelection)} beats ${computerSelection}.`
+            );
+        } else if (playerScore === 3) {
+            displayResults(
+                `${capitalize(playerSelection0)} beats ${computerSelection}! Has mankind found its savior??
+                `
+            );
+        } else if (playerScore === 4) {
+            displayResults(
+                `${capitalize(
+                    playerSelection)} beats ${computerSelection}. One more and you're a hero!
+                `
+            );
+        } else {
+            displayResults(`${playerSelection} beats ${computerSelection}`);
+        }
+    }
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function displayResults(str) {
+    container.animate([{ opacity: 0}, { opacity: 1}]. {
+        duration: 300,
+        fill: "forwards",
+        iterations: 1,
+        delay: 0,
+        easing: "ease-out",
+        });
+        container.textContent = str;
+}
+
+function declareWinner() {
+    rplContent();
+    if (playerScore > computerScore) {
+        endDesc.textContent = "You win! Mankind lives another day!!";
+        returnMainBtn.innerText = "Play again";
+    } else {
+        endDesc.textContent = "You lost..who will save mandkind now?";
+        returnMainBtn.innerText = "Try again?";
+    }
+    fadeIn();
+
+    let endDescSpan = endDesc.querySelectorAll("span");
+    endDescSpan = Array.from(endDescSpan);
+
+    endDescSpan[endDescSpan.length - 1].ontransitioned = () => {
+        returnMainBtn.classList.add("fadeIn");
+    };
+}
+
+function rplContent() {
+    main.classList.add("disappear");
+    endAlrt.classList.add("disappear");
+    desc.classList.remove("animate");
+    endDescS.classList.add("animate");
+
+    returnMainBtn.addEventListener("click", () => {
+        main.classList.remove("disappear");
+        endAlrt.classList.add("disappear");
+        desc.classList.remove("animate");
+        returnMainBtn.classList.remove("fadeIn");
+        resetGame();
+    });
+}
+
+fucntion resetGame() {
+    fadeIn();
+    container.textContent = "";
+    playerScore = 0;
+    computerScore = 0;
+    keepPlayerScore = 0;
+    keepCpuScore = 0;
+}
+
+function keepPlayerScore() {
+    let playerScoreBox = document.querySelector("#player-score");
+
+    playerScoreBox.animate([{ opacity: 0 }, { opacity: 1}], {
+        duration: 300,
+        fill: "forwards",
+        iterations: 1,
+        delay: 0,
+        easing: "ease-out",
+    });
+
+    playerScoreBox.textContent = playerScore;
+}
+
+function keepCpuScore() {
+    let computerScoreBox = document.querySelector("#computer-score");
+
+    computerScoreBox.animate([{ opacity: 0}, { opacity: 1}], {
+        duration: 300,
+        fill: "forwards",
+        iterations: 1,
+        delay: 0,
+        easing: "ease-out",
+    });
+
+    computerScoreBox.textContent = computerScore;
+}
